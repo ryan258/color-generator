9de49033a9117d01ab230f206d19e1095a179afc
Ryan Johnson <ryanleejwebdev@gmail.com>
2020-11-22 10:55:28 -0600

    Set up structure.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
-import React, { useState } from 'react'
-import SingleColor from './SingleColor'
+import React, { useState } from 'react';
+import SingleColor from './SingleColor';
 
-import Values from 'values.js'
+import Values from 'values.js';
 
 function App() {
-  return <h2>color generator project</h2>
+  const [color, setColor] = useState('');
+  const [error, setError] = useState(false);
+  const [list, setList] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('moo');
+  };
+
+  return (
+    <>
+      <section className="container">
+        <h3>color generator</h3>
+        <form onSubmit={handleSubmit}>
+          {/* controlled input */}
+          <input
+            type="text"
+            value={color}
+            name=""
+            id=""
+            onChange={(e) => setColor(e.target.value)}
+            placeholder="#c9a328"
+          />
+          <button className="btn" type="submit">
+            submit
+          </button>
+        </form>
+      </section>
+      <section className="colors">
+        <h4>list goes here</h4>
+      </section>
+    </>
+  );
 }
 
-export default App
+export default App;
