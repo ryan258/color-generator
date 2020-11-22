799847256d2252dcd12fcf05c44c205248ce4916
Ryan Johnson <ryanleejwebdev@gmail.com>
2020-11-22 12:19:00 -0600

    Add click/copy hex to clipboard functionality.

--- src/SingleColor.js
+++ src/SingleColor.js
@@ -1,12 +1,20 @@
 import React, { useState, useEffect } from 'react';
-import rgbToHex from './utils';
+// import rgbToHex from './utils';
 
 const SingleColor = ({ rgb, weight, index, hexColor, type }) => {
   // console.log(hexColor);
   const [alert, setAlert] = useState(false);
   const bcg = rgb.join(',');
-  const hex = rgbToHex(...rgb);
+  // const hex = rgbToHex(...rgb);
   const hexValue = `#${hexColor}`;
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setAlert(false);
+    }, 3000);
+
+    return () => clearTimeout(timeout);
+  }, [alert]);
 
   return (
     <article
@@ -14,9 +22,14 @@ const SingleColor = ({ rgb, weight, index, hexColor, type }) => {
         (type === 'base' || type === 'shade') && 'color-light'
       }`}
       style={{ backgroundColor: `rgb(${bcg})` }}
+      onClick={() => {
+        setAlert(true);
+        navigator.clipboard.writeText(hexColor);
+      }}
     >
       <p className="percent-value">{weight}%</p>
       <p className="color-value">{hexValue}</p>
+      {alert && <p className="alert">copied to clipboard</p>}
     </article>
   );
 };
